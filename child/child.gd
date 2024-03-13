15f2732764f24f0331d47bc5949b5f5b8be0a2e0
ClaraAkmal <claraakmalwadie2004@gmail.com>
2024-03-13 14:24:16 -0700

    Update child.gd

--- child/child.gd
+++ child/child.gd
@@ -10,23 +10,10 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	var direction = Input.get_axis("left", "right")
 	
-	if direction:
-		velocity.x = direction  * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	var d = Input.get_axis("down","up")
-	
-	if d:
-		velocity.y = d  * -SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
- 
-
+	var d = Input.get_vector("right","left","down","up")
+	velocity = d * -SPEED
 	move_and_slide()
-
 
 func _on_game_body_entered(body):
 	print("HI")
